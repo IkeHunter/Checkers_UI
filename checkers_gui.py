3c2dc81248f2475dab11d8afd70b53e749ded792
Isaac Hunter <ikehunter5@gmail.com>
2019-08-04 00:51:05 -0400

    formated board

--- checkers_gui.py
+++ checkers_gui.py
@@ -4,13 +4,7 @@ import tkinter
 class CheckersGUI:
 
     def __init__(self):
-        # self.mainWindow = tkinter.Tk()
-        # self.mainWindow.title("Checkers")
-        # self.mainWindow.geometry('900x900-80-100')
         self.board = CheckerBoard()
-        # self.config()
-        # self.board_setup()
-        # self.mainWindow.mainloop()
 
     def config(self, main_window):
         board_obj = self.board.get_board()
@@ -23,32 +17,30 @@ class CheckersGUI:
         self.board = self.board.start_board()
         board_grid = dict()
 
-        def add_box(grid, box_var, i):  # i=board_row, j=board_col
-            if grid[i] == 0:
-                grid[i] = [box_var]
-            else:
-                # grid.append(box_var)
-                grid[i] = [box_var]
+        def add_box(grid, box_var, i):
+            grid[i] = [box_var]
+
             return grid
 
         for board_row in range(len(self.board.keys())):
-            print("{}: {}".format(board_row, board_grid))
             board_grid.update({board_row: 0})
             for board_col in range(len(self.board[board_row])):
                 if self.board[board_row][board_col] == 5:
-                    box = tkinter.Label(main_window, padx=5, pady=5, bg='black')\
+                    box = tkinter.Label(main_window, padx=12, pady=12, bg='black', text=" ")\
                         .grid(row=board_row, column=board_col, sticky='nsew')
-                    # if board_grid[board_row].get() is None:
-                    #     board_grid[board_row] = [box]
-                    # else:
-                    #     board_grid[board_row].append(box)
                     board_grid = add_box(board_grid, box, board_row)
                 elif self.board[board_row][board_col] == 0:
-                    box = tkinter.Label(main_window, padx=5, pady=5, bg='white')\
+                    box = tkinter.Label(main_window, padx=12, pady=12, bg='white', text=" ")\
                         .grid(row=board_row, column=board_col, sticky='nsew')
                     board_grid = add_box(board_grid, box, board_row)
                 elif self.board[board_row][board_col] == 1:
-                    box = tkinter.Label(main_window, padx=5, pady=5, bg='white', text='O', font=("Helvetica", 30))\
+                    text_opts = ("Helvetica", 35, 'bold')
+                    box = tkinter.Label(main_window, padx=0, pady=0, bg='white', text='O', font=text_opts, fg='blue')\
+                        .grid(row=board_row, column=board_col, sticky='nsew')
+                    board_grid = add_box(board_grid, box, board_row)
+                elif self.board[board_row][board_col] == 2:
+                    text_opts = ("Helvetica", 35, 'bold')
+                    box = tkinter.Label(main_window, padx=0, pady=0, bg='white', text='O', font=text_opts, fg='red')\
                         .grid(row=board_row, column=board_col, sticky='nsew')
                     board_grid = add_box(board_grid, box, board_row)
 
@@ -57,8 +49,6 @@ class CheckersGUI:
         main_window.geometry('900x900-80-100')
         self.config(main_window)
         self.board_setup(main_window)
-        # main_window.mainloop()
-
 
 
 class CheckerBoard:
@@ -82,10 +72,16 @@ class CheckerBoard:
 
     def start_board(self):
         for i in range(len(self.board_dict.keys())):
-            if i <= 2 or i >= 5:
+            # if i <= 2 or i >= 5:
+            if i <= 2:
                 for j in range(len(self.board_dict[i])):
                     if self.board_dict[i][j] == 0:
                         self.board_dict[i][j] = 1
+            elif i >= 5:
+                for j in range(len(self.board_dict[i])):
+                    if self.board_dict[i][j] == 0:
+                        self.board_dict[i][j] = 2
+
         return self.board_dict
 
     def print_board(self):
--- main.py
+++ main.py
@@ -6,22 +6,9 @@ def main():
     checkers = gui.CheckersGUI()
 
     main_window = tkinter.Tk()
-    main_window.title("Checkers")
-    main_window.geometry('900x900-80-100')
     checkers.main_loop(main_window)
     main_window.mainloop()
 
-    # return checkers
-
 
 if __name__ == '__main__':
-    # main_window = tkinter.Tk()
-    # main_window.title("Checkers")
-    # main_window.geometry('900x900-80-100')
-    # main_window.rowconfigure(0, weight=1)
-    # main_window.columnconfigure(0, weight=1)
-    # some_label = tkinter.Label(main_window, text="hello")
-    # some_label.grid(row=0, column=0)
-    # # checkers.main_loop(main_window)
-    # main_window.mainloop()
     main()
