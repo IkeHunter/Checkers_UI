62dda9940a3705c3b16af9524afb7d944f464fce
Isaac Hunter <ikehunter5@gmail.com>
2019-08-17 23:21:03 -0400

    added bridge class

---
+++ checkers_functions.py
@@ -0,0 +1,19 @@
+import checkers_game as cg
+
+
+class CheckersBridge:
+
+    def __init__(self):
+        self.game = cg.CheckerBoard()
+
+    def reset(self):
+        pass
+
+    def render(self):
+        pass
+
+    def step(self):
+        pass
+
+    def has_won(self):
+        pass
